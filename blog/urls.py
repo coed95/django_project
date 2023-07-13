32c2045b7148488673ead7218fbfb89f33f57db3
coed95 <codecoed95@gmail.com>
2023-07-13 17:07:35 +0200

    PostDetailView and primary key added

--- blog/urls.py
+++ blog/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 from . import views
-from .views import PostListView
+
 
 urlpatterns = [
     path('', PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
     path('register/', views.about, name='blog-register'),
 ]
