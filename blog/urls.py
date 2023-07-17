3a3b2ce2f8f06eca0bfdf25431666cb73cc8fb60
coed95 <codecoed95@gmail.com>
2023-07-17 15:24:52 +0200

    fixed a NameError

--- blog/urls.py
+++ blog/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 
 urlpatterns = [
-    path('', PostListView.as_view(), name='blog-home'),
-    path('post/<int:pk>/', PostDetailView.as_view(), name='post-detail'),
+    path('', views.PostListView.as_view(), name='blog-home'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
     path('about/', views.about, name='blog-about'),
     path('register/', views.about, name='blog-register'),
 ]
